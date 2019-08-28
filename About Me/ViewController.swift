5f905b3c5f6bb9e4ac5d1c39d5a13cd09bbfdcac
Mokun Li <42788001+libomo@users.noreply.github.com>
2019-08-27 23:00:31 -0400

    Add files via upload

---
+++ About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Mokun on 2019/8/27.
+//  Copyright © 2019 Mokun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
